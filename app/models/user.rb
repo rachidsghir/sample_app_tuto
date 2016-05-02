24ed566d73e42affdd6d0e9c86dda37c82466ce7
rachid sghir <sghirr@gmail.com>
2016-05-02 18:50:10 +0100

    Added password_digest column to users table and install the bcrypt gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ActiveRecord::Base
+  # Berfore Save Method
+  before_save {self.email = email.downcase}
 
-  #Contants
+  # Contants
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\.-_]+\.[a-z]+\z/i
 
-  #Validations
+  # Validations
   validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true, length: {maximum: 255},format: {with: VALID_EMAIL_REGEX, message: "You should enter a valid email"},uniqueness: {case_sensitive: false, message:"this email has already existed, try with another email"}
+  validates :password, presence: true, length: {minimum: 6}
+  # Secure Password
+
+  has_secure_password
 
 end
